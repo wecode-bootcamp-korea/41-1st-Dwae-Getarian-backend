5060aa66153b15aab71dd89ed2b8318b036e8a72
Gunho <paulryu1998@gmail.com>
2023-01-10 21:01:40 +0900

    modify table setting

--- db/migrations/20230110113711_modify_orders_status_table.sql
+++ db/migrations/20230110113711_modify_orders_status_table.sql
@@ -3,5 +3,7 @@ ALTER TABLE `order_product` DROP FOREIGN KEY order_product_order_status_fk;
 ALTER TABLE `order_status` MODIFY `id` INT NOT NULL AUTO_INCREMENT;
 ALTER TABLE `order_product` ADD CONSTRAINT order_product_order_status_fk FOREIGN KEY (order_status_id)
 REFERENCES order_status (id) ON DELETE CASCADE;
+ALTER TABLE `order_product` MODIFY `order_status_id` INT NOT NULL DEFAULT 1;
+
 -- migrate:down
 

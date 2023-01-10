14d66261fb4c4a6ece19bfdc585849bc0701d156
Gunho <paulryu1998@gmail.com>
2023-01-10 15:03:11 +0900

    applied suggested changes

--- api/models/orderDao.js
+++ api/models/orderDao.js
@@ -2,51 +2,46 @@ const { appDataSource } = require("../database/database");
 
 
 async function createOrderTable(userId, totalCost) {
-    try {
-			const orderTable = await appDataSource.query(
-				`
-					INSERT INTO orders 
-						(user_id, total_cost) 
-					VALUES (?, ?)
-    		`, [ userId, totalCost ]);
-
-    return orderTable;
-        
-    } catch(err) {
-        throw err;
-    } 
+	try {
+		return await appDataSource.query(
+			`
+			INSERT INTO orders 
+				(user_id, total_cost) 
+			VALUES (?, ?)
+			`, [ userId, totalCost ]);
+			
+	} catch(err) {
+			throw err;
+	} 
 }
 
 async function createOrder(orderId, products) {
     try {
-        const query = 
-				`
-					INSERT INTO order_product 
-						(orders_id, product_id, quantity)
-        	VALUES ?;
-        `
-        const values = products.map((product) => {
-            return [ orderId, product.id, product.quantity ];
-        });
-    
-        const requestResult = await appDataSource.query(query, [values]);
+			const values = products.map((product) => {
+				return [ orderId, product.id, product.quantity ];
+		});
 
-        return requestResult;
+		return appDataSource.query(
+		`
+		INSERT INTO order_product (
+			orders_id, 
+			product_id, 
+			quantity
+		) VALUES ?;
+		`, [values]);
 
     } catch(err) {
-        throw err;
+      throw err;
     }
 }
 
 async function deleteOrder(userId, orderId) {
     try {
-			const requestResult = await appDataSource.query(
+			return await appDataSource.query(
 				`
 					DELETE FROM orders
 					WHERE user_id = ? AND order_id = ?
 				`, [ userId, orderId ]);
-	
-			return requestResult;
 			
 		} catch (err) {
 			throw err;

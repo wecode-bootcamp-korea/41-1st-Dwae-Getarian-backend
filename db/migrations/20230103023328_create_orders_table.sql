91edd5f574d0a4518eaa4e9201c1c37ec73a08f9
Gunho <paulryu1998@gmail.com>
2023-01-10 21:32:51 +0900

    modify table

--- db/migrations/20230103023328_create_orders_table.sql
+++ db/migrations/20230103023328_create_orders_table.sql
@@ -2,6 +2,7 @@
 CREATE TABLE `orders` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id` INT NOT NULL,
+	`total_cost` INT NOT NULL,
   `order_status_id` INT NOT NULL,
   CONSTRAINT orders_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id),
   CONSTRAINT orders_status_id_fk FOREIGN KEY (order_status_id) REFERENCES orders (id)

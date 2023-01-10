17dfce0fca9b0c425a3138a7051a9487f617a935
Gunho <paulryu1998@gmail.com>
2023-01-11 02:44:12 +0900

    git checkout

--- api/middlewares/errorHandler.js
+++ api/middlewares/errorHandler.js
@@ -1,17 +1,11 @@
-function asyncErrorHandler(func) {
-	return async(req, res, next) => {
-		func(req, res).catch((err) => next(err));
-	}
-}
-
 function globalErrorHandler (err, req, res, next) {
-  const statusCode = 500 || err.statusCode;
     
-  res.status(statusCode).json({ message: err.message });
+    const statusCode = 500 || err.statusCode;
+    
+    res.status(statusCode).json({ message: err.message });
 }
 
 
 module.exports = {
-	asyncErrorHandler,
-  globalErrorHandler
+    globalErrorHandler
 }

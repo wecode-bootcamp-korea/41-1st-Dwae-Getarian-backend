1bffa56e7156a13239b1e85f5e66ee7a307ad909
Gunho <paulryu1998@gmail.com>
2023-01-11 09:57:40 +0900

    git refactored the very last bit

--- api/controllers/orderController.js
+++ api/controllers/orderController.js
@@ -5,9 +5,7 @@ async function createOrder(req, res) {
 	const userId = req.userId; 
 	const orderData = req.body;
 	
-	if (!orderData || !userId) {
-		detectError("NO INPUT DATA", 401);
-	}
+	if (!orderData || !userId) detectError("NO INPUT DATA", 401);
 	
 	await orderService.createOrder(userId, orderData);
 
@@ -20,10 +18,8 @@ async function deleteOrder(req, res) {
 	const orderId = req.query.orderId;
 	const { totalCost, totalCo2 } = req.body;
 
-	if (!orderId || !totalCost || !totalCo2) {
-		detectError("INPUT ERROR", 401);
-	}
-
+	if (!orderId || !totalCost || !totalCo2) detectError("INPUT ERROR", 401);
+	
 	await orderService.deleteOrder(userId, orderId, totalCost, totalCo2);
 
 	return res.status(200).json({ message: "DELETING THE ORDER SUCCESSFULLY HANDLED" });

be9fb7d44ab091fc8d92acc74cb24e3cafefee0c
Gunho <paulryu1998@gmail.com>
2023-01-23 18:58:31 +0900

    final

--- api/models/cartDao.js
+++ api/models/cartDao.js
@@ -1,79 +1,68 @@
 const { appDataSource } = require("../database/database");
 
 async function insertCartItem(userId, productId, quantity) {
-	const checkResult = await checkCartItems(userId, productId);
+	return await appDataSource.query(`
+		INSERT INTO CART 
+		 (user_id, product_id, quantity)
+		VALUES
+		 (${userId}, ${productId}, ${quantity})
+		ON DUPLICATE KEY UPDATE 
+			quantity = quantity + ${quantity}
+		`)
+}
 
-	if (!checkResult) {
-    const requestCartData = await appDataSource.query(`
-			INSERT INTO cart
-				(user_id, product_id, quantity)
-			VALUES 
-				(${userId}, ${productId}, ${quantity})
-		`);
-	
-			return requestCartData;
-	} else {
-		const requestCartData = await appDataSource.query(`
-			UPDATE cart
-				SET quantity = ${quantity + checkResult.quantity}
-			WHERE id = ${checkResult.id};
-		`);
-	
-		return requestCartData;
-	}
+async function updateCartItems(userId, productId, quantity) {
+	await appDataSource.query(`
+	UPDATE cart
+		SET quantity = ${quantity} 
+	WHERE product_id = ${productId}
+	AND user_id = ${userId};
+`, );
 }
 
 async function getCartItem(userId) {
-	return await appDataSource.query(`
+	try {
+		return await appDataSource.query(`
 		SELECT 
 			JSON_ARRAYAGG(JSON_OBJECT(
 			"id", p.id, 
 			"name", p.name, 
 			"image", p.thumbnail_image, 
 			"price", p.price, 
-			"category", c.quantity)) AS data
+			"quantity", c.quantity)) AS data
 		FROM cart c
 		INNER JOIN users u ON u.id = c.user_id
 		INNER JOIN products p ON p.id = c.product_id
 		WHERE u.id = ${userId}
 		GROUP BY u.id;
 	`);
-}
-
-async function checkCartItems(userId, productId) {
-	const checkItems = await appDataSource.query(`
-		SELECT * FROM cart c
-		WHERE c.user_id = ${userId} AND c.product_id = ?
-	`, [ productId ]);
-
-		if (checkItems.length === 0) {
-			return 0;
-		} 
-
-		const dataObj = {
-			id: checkItems[0].id,
-			quantity: checkItems[0].quantity
-		}
-
-		return dataObj;
-} 
-
-async function deleteCartItems(userId, cartItems) {
-	const values = [];
-
-	for (let i = 0; i < cartItems.length; i++) {
-		values.push(cartItems[i].id)
+	} catch(err) {
+		throw err;
 	}
 
-	return await appDataSource.query(`
-		DELETE FROM cart 
-		WHERE cart.user_id = ${userId} 
-		AND cart.id IN (?)`
-	, [ values ])
+}
+
+async function deleteCartItems(userId, cartItems) {
+	try{
+		const values = [];
+
+		for (let i = 0; i < cartItems.length; i++) {
+			values.push(cartItems[i].id)
+		}
+		
+		return await appDataSource.query(`
+			DELETE FROM cart 
+			WHERE cart.user_id = ${userId} 
+			AND cart.product_id IN (?)`
+		, [ values ])
+	} catch(err) {
+		throw err;
+	}
 }
 
 module.exports = {
 	insertCartItem,
 	getCartItem,
-	deleteCartItems
+	deleteCartItems,
+	updateCartItems
 }

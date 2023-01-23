6d533b58e8cf9868d037e85e78778b28e5a7d494
Gunho <paulryu1998@gmail.com>
2023-01-23 18:58:31 +0900

    final

--- api/controllers/cartController.js
+++ api/controllers/cartController.js
@@ -9,32 +9,43 @@ async function insertCartItem(req, res) {
 
 	await cartService.insertCartItem(userId, productId, quantity);
 
-	return res.status(201).json({ message: "PRODUCT UPDATED SUCCESSFULLY!!! (cartController)"});
+	return res.status(201).json({ message: "PRODUCT UPDATED SUCCESSFULLY!!!"});
 }
 
 async function getCartItems(req, res) {
 	const userId = req.userId;
 	const cartItems = await cartService.getCartItem(userId);
 
-	if (!cartItems.length) detectError("NO FOLLOWING PRODUCTS TRY PUTTTING SOME ITEMS!!!", 401);
-	
-	return res.status(200).json(cartItems);
+	if (cartItems.length) return res.status(200).json(cartItems[0]["data"]);
+	else return res.status(400).json([]);
 }
 
-async function deleteCartItems(req, res) {
+async function updateCartItems(req, res) {
+	const { productId, quantity } = req.body;
 	const userId = req.userId;
-	const cartItems = req.body["cart_items"];
 
-	if (!cartItems) detectError("NO INPUT", 401);
+	if (!productId || !quantity) return detectError("NO ITEMS TO BE UPDATED", 401); 
+	
+	await cartService.updateCartItems(userId, productId, quantity);
+
+	res.status(201).json("CART ITEMS UPDATED SUCCESSFULLY!");
+}
+
+async function deleteCartItems(req, res) {    
+	const userId = req.userId;
+	const cartItems = req.body;
+
+	if (!cartItems.length) detectError("NO INPUT", 401);
 
 	await cartService.deleteCartItems(userId, cartItems);
 
-	return res.status(200).json({ message: "delete request handled successfully" })
+	return res.status(200).json({ message: "DELETE REQUEST HANDLED SUCCESSFULLY" })
 } 
 
 
 module.exports = {
 	insertCartItem,
 	getCartItems,
-	deleteCartItems
+	deleteCartItems,
+	updateCartItems
 }
